23e00c45c039b8ecb5b265f8c26b237e72f2adf3
Woopsyyy <yoshiru28@gmail.com>
2026-08-25 15:19:38 +0800

    fix: hide splash screen after layout mounts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -6,6 +6,7 @@ import {
 } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 import { useColorScheme } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
@@ -15,6 +16,10 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>

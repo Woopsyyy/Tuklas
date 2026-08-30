2ced60f5c926de89e0f84ec659394ca0e1c79b79
Woopsyyy <yoshiru28@gmail.com>
2026-08-30 12:21:25 +0800

    feat: move antas5-slide1 next button left by 5%

--- src/app/antas5-slide1.tsx
+++ src/app/antas5-slide1.tsx
@@ -38,7 +38,7 @@ export default function Antas5Slide1Screen() {
   // Source 128x56
   const nextW = 420 * bgScale * 0.8;
   const nextH = nextW * (56 / 128);
-  const nextLeft = (screenW - nextW) / 2 + 0.50 * screenW - 0.10 * screenW;
+  const nextLeft = (screenW - nextW) / 2 + 0.50 * screenW - 0.10 * screenW - 0.05 * screenW;
   const nextTop = signTop + signH + 60 * bgScale + 0.20 * screenH - 0.05 * screenH;
 
   useEffect(() => {

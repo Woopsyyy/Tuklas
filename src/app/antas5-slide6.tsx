3cb806bf49f2059a090809c859164ed5c4c89409
Woopsyyy <yoshiru28@gmail.com>
2026-08-30 23:45:47 +0800

    feat: move npc down by 5% on antas5 slide6

--- src/app/antas5-slide6.tsx
+++ src/app/antas5-slide6.tsx
@@ -51,7 +51,7 @@ export default function Antas5Slide6Screen() {
   const npcH = screenH * 0.80 * 1.5 * 0.5 * 1.5 * 1.4;
   const npcW = npcH * (330 / 1033);
   const npcLeft = bgOffsetX - npcW * 0.30 + 0.10 * screenW;
-  const npcTop = (screenH - npcH) / 2;
+  const npcTop = (screenH - npcH) / 2 + 0.05 * screenH;
 
   useEffect(() => {
     ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);

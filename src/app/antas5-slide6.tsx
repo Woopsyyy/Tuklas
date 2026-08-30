f293aaf161c98781e5522769a371a49de2cae8dc
Woopsyyy <yoshiru28@gmail.com>
2026-08-30 23:48:14 +0800

    feat: move npc right by 3% on antas5 slide6

--- src/app/antas5-slide6.tsx
+++ src/app/antas5-slide6.tsx
@@ -50,7 +50,7 @@ export default function Antas5Slide6Screen() {
   // Source 330x1033
   const npcH = screenH * 0.80 * 1.5 * 0.5 * 1.5 * 1.4 * 0.9;
   const npcW = npcH * (330 / 1033);
-  const npcLeft = bgOffsetX - npcW * 0.30 + 0.10 * screenW;
+  const npcLeft = bgOffsetX - npcW * 0.30 + 0.10 * screenW + 0.03 * screenW;
   const npcTop = (screenH - npcH) / 2 + 0.05 * screenH + 0.05 * screenH;
 
   useEffect(() => {

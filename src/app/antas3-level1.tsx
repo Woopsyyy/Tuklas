fed5e70f08d0971813bdad6547a938313fb9bda6
Woopsyyy <yoshiru28@gmail.com>
2026-09-02 19:30:10 +0800

    feat: move antas3-level1 next and back buttons left by another 10%

--- src/app/antas3-level1.tsx
+++ src/app/antas3-level1.tsx
@@ -226,14 +226,14 @@ useFocusEffect(
         </Animated.View>
 
         {/* Signpost Next */}
-        <Animated.View entering={FadeIn.duration(600)} style={{ position: "absolute", left: signLeft - 0.03 * screenW + signW * 1.44, top: nextTop * 0.98, width: signW, height: nextH, zIndex: 25 }}>
+        <Animated.View entering={FadeIn.duration(600)} style={{ position: "absolute", left: signLeft - 0.03 * screenW + signW * 1.34, top: nextTop * 0.98, width: signW, height: nextH, zIndex: 25 }}>
           <Pressable onPress={handleNext} hitSlop={8} className="w-full h-full active:scale-95 active:opacity-80">
             <RNImage source={require("../../assets/images/ui/next.png")} style={{ width: "100%", height: "100%" }} resizeMode="contain" />
           </Pressable>
         </Animated.View>
 
         {/* Signpost Back */}
-        <Animated.View entering={FadeIn.duration(600)} style={{ position: "absolute", left: signLeft - 0.03 * screenW + signW * 1.42, top: backTop * 1.02, width: signW * 0.95, height: backSignH * 0.95, zIndex: 25 }}>
+        <Animated.View entering={FadeIn.duration(600)} style={{ position: "absolute", left: signLeft - 0.03 * screenW + signW * 1.32, top: backTop * 1.02, width: signW * 0.95, height: backSignH * 0.95, zIndex: 25 }}>
           <Pressable onPress={handleBack} hitSlop={8} className="w-full h-full active:scale-95 active:opacity-80">
             <RNImage source={require("../../assets/images/ui/back-green.png")} style={{ width: "100%", height: "100%" }} resizeMode="contain" />
           </Pressable>

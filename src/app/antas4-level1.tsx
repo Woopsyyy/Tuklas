3dc71604c48bc7f3d7da7e9719ff4409816b1cae
Woopsyyy <yoshiru28@gmail.com>
2026-09-02 20:04:35 +0800

    feat: antas4-level1 move next right 3% and back right 3% and up 2%

--- src/app/antas4-level1.tsx
+++ src/app/antas4-level1.tsx
@@ -765,7 +765,7 @@ export default function Antas4Level1Screen() {
           entering={FadeIn.duration(600)}
           style={{
             position: "absolute",
-            left: signLeft - 0.05 * screenW + 0.02 * screenW + signW * 1.83 - 0.05 * screenW,
+            left: signLeft - 0.05 * screenW + 0.02 * screenW + signW * 1.86 - 0.05 * screenW,
             top: nextTop * 1.03 - 0.04 * screenH,
             width: signW,
             height: nextH,
@@ -790,8 +790,8 @@ export default function Antas4Level1Screen() {
           entering={FadeIn.duration(600)}
           style={{
             position: "absolute",
-            left: signLeft - 0.03 * screenW + 0.01 * screenW + signW * 1.73 - 0.05 * screenW,
-            top: backTop * 1.03 * 1.05 * 0.97 * 1.02 - 0.04 * screenH,
+            left: signLeft - 0.03 * screenW + 0.01 * screenW + signW * 1.76 - 0.05 * screenW,
+            top: backTop * 1.03 * 1.05 * 0.97 * 1.02 * 0.98 - 0.04 * screenH,
             width: signW * 0.95,
             height: backSignH * 0.95,
             zIndex: 35,

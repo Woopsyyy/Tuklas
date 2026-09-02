0dd0e5c59e83f33fbfed83e28a9c092db5fd9537
Woopsyyy <yoshiru28@gmail.com>
2026-09-02 16:09:32 +0800

    fix: move next and green back button left by 5% on antas1-level1

--- src/app/antas1-level1.tsx
+++ src/app/antas1-level1.tsx
@@ -254,7 +254,7 @@ export default function Antas1Level1Screen() {
           entering={FadeIn.duration(600)}
           style={{
             position: "absolute",
-            left: signLeft - 0.05 * screenW + 0.02 * screenW + 0.01 * screenW + 0.05 * screenW - 0.05 * screenW + signW * 1.8,
+            left: signLeft - 0.05 * screenW + 0.02 * screenW + 0.01 * screenW + 0.05 * screenW - 0.05 * screenW + signW * 1.8 - 0.05 * screenW,
             top: (nextTop + 0.005 * screenH + 0.02 * screenH - 0.01 * screenH) * 0.97335,
             width: signW * 1.2,
             height: nextH * 1.2,
@@ -279,7 +279,7 @@ export default function Antas1Level1Screen() {
           entering={FadeIn.duration(600)}
           style={{
             position: "absolute",
-            left: signLeft - 0.05 * screenW + 0.02 * screenW + 0.01 * screenW + 0.05 * screenW - 0.05 * screenW + signW * 1.8,
+            left: signLeft - 0.05 * screenW + 0.02 * screenW + 0.01 * screenW + 0.05 * screenW - 0.05 * screenW + signW * 1.8 - 0.05 * screenW,
             top: backTop + 0.01 * screenH + 0.02 * screenH - 0.01 * screenH,
             width: signW * 0.98 * 0.98 * 0.98 * 1.2,
             height: backH2 * 0.98 * 0.98 * 0.98 * 1.2,
